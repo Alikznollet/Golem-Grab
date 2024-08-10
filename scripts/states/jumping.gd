6f8970c97ed0de6ae616978d5e1ac09ba3f7d35f
Alikznollet <alexanderacj13@gmail.com>
2024-08-10 22:17:42 +0200

    implemented jump cut

--- scripts/states/jumping.gd
+++ scripts/states/jumping.gd
@@ -10,5 +10,8 @@ func Physics_Update(delta: float) -> void:
 	player.velocity.y += player.jump_gravity * delta
 	player.handle_input_velocity()
 	
+	if Input.is_action_just_released("jump"):
+		player.velocity.y = player.velocity.y / 2
+	
 	if player.velocity.y >= 0:
 		Transition.emit(self, "falling")
